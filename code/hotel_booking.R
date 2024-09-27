e4bf5c20e6318ea17bd6b8ad40f40bd4d1b7ef58
andre-11c <andrechipanarios.11@gmail.com>
2024-09-27 00:56:47 -0500

    actualizar data de script identifiacion eliminado y reemplazado

--- code/hotel_booking.R
+++ code/hotel_booking.R
@@ -1,14 +1,4 @@
 
-#identificacion y tratamiento de datos faltantes
-
-df<-read.csv("C:/Users/Andre/Desktop/data/hotel_bookings.csv", sep=",")
-# Para contar los Na totales en todo el dataset
-sum(is.na(df))
-#para contar los NA en cada colummna
-colSums(is.na(df))
-
-# Mostrar las filas y columnas que contienen NA
-which(is.na(df), arr.ind = TRUE)
 
 # Cargar librerías necesarias
 library(tidyverse) # Para manipulación de datos y gráficos
